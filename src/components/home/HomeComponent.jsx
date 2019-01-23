863bd2cd6ba16cbbe1f014c981631e90d6ad0053
Kelsey Ando <kelsey.ando@detroitlabs.com>
2019-01-23 11:16:03 -0500

    adding code to get the current location from the user on the home page

--- src/components/home/HomeComponent.jsx
+++ src/components/home/HomeComponent.jsx
@@ -1,15 +1,32 @@
 import React, {Component} from 'react';
-import * as Constants from '../../util/constants';
 
 class HomeComponent extends Component {
+  state = {
+    location: {
+      lat: 42.3352587,
+      long: -83.04943689999999
+    },
+    locationUpdate: false
+  }
 
   componentDidMount() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      this.setState({location: {lat: location.coords.latitude, long: location.coords.longitude}, locationUpdate:true})
+    })
   }
 
   render() {
+    const {location, locationUpdate} = this.state;
     return (
       <div>
-        <h1> Home Component</h1>
+        {!locationUpdate && 
+          <h2>Default Location: Detroit</h2>
+        }
+        {locationUpdate && 
+          <h2>Current Longitude and Latitude:</h2>
+        }
+        <h3>Latitude: {location.lat}</h3>
+        <h3>Longitude: {location.long}</h3>
       </div>
     )
   }
