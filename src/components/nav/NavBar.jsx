8f4e56f89013ed290cb52dd4db6e7084fa82e54f
Kelsey Ando <kelsey.ando@detroitlabs.com>
2019-01-30 14:28:45 -0500

    adding style sheets and some styling for the navbar

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { NavLink} from 'react-router-dom';
 import DropDown from '../dropDown';
+import '../../styles/navbar.css';
 
 class NavBar extends Component {
   render() {
     return (
-      <div>
+      <div className="nav">
         <NavLink to='/current'>Current</NavLink>
         <NavLink to='/fiveday'>Five Day</NavLink>
         <DropDown/>
-        
       </div>
     )
   }
