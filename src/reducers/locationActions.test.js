f2da9f0155bfc3124334e29221d81d4a4f852fe2
Kelsey Ando <kelsey.ando@detroitlabs.com>
2019-02-01 14:46:22 -0500

    adding tests for the actions

--- src/reducers/locationActions.test.js
+++ src/reducers/locationActions.test.js
@@ -2,12 +2,36 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { configureStore} from '../store/configStore';
+import {updateLocation, updateLocationUpdated, updateUnit } from './locationActions';
 
 const store = configureStore();
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe("location Reducer", () => {
-  it('', () => {
+const updatedLocation = {
+  lat: 37.5407,
+  long: -77.4360
+};
+
+const update = {
+  locationUpdated: true
+};
+
+
+describe("location Actions", () => {
+  it('action creator updateLocation', () => {
+    const location = updateLocation(updatedLocation);
+    expect(location).toEqual({type:"UPDATE_LOCATION", payload: {location:updatedLocation}});
   });
+
+  it('action creator updateLocationUpdated', () => {
+    const location = updateLocationUpdated(true);
+    expect(location).toEqual({type:"UPDATE_LOCATION_UPDATED", payload: update});
+  });
+
+  it('action creator updateUnit', () => {
+    const location = updateUnit("metric");
+    expect(location).toEqual({type:"UPDATE_UNIT", payload: {unit: "metric"}});
+  });
+
 });
